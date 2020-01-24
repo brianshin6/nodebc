4f9948b042a176de784371703dd1485b7605ddfe
Brian Shin <brianshin6@gmail.com>
2020-01-23 19:50:04 -0800

    Added completed CSS exercises

---
+++ js/index1.js
@@ -0,0 +1,5 @@
+var first_name;
+var last_name;
+var age;
+var address;
+var dateOfBirth;
---
+++ js/index2.js
@@ -0,0 +1,11 @@
+var first_name;
+var last_name;
+var age;
+var date_of_birth;
+var address;
+
+first_name = 'Brian'
+last_name = 'Shin'
+age = '29'
+date_of_birth = '12/16/1990'
+address = '1008 Gilroy Crescent'
