c3dafae99339960c6bb1aceb6c5e18ea86be4927
Brian Shin <brianshin6@gmail.com>
2020-01-24 20:06:06 -0800

    Completed JavaScript Exercises

---
+++ js/index12.js
@@ -0,0 +1,5 @@
+let firstname = 'Brian'
+let greeting = 'Welcome'
+console.log(firstname);
+console.log(greeting);
+console.log(greeting + ' ' + firstname);
---
+++ js/index14.js
@@ -0,0 +1,4 @@
+let firstname = 'Brian'
+let greeting = 'Welcome'
+let endOfMessage = 'to learning JavaScript with friends'
+console.log(greeting + ' ' + firstname + ' ' + endOfMessage)
